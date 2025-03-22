ea4936601845555032b5b9ea7f15783b58d458d0
ap2501 <work.amit2501@gmail.com>
2025-03-22 18:55:57 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '87b3ddff-d8fe-4275-b1c2-1fa5fc877c32';
+export const MODAL_CLOSE_SEC = 2.5;
